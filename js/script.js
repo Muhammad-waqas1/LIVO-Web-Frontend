ee25c565063135f8e7491dd62f1f2bd3207ccf75
Waqas <waqas.print.404@gmail.com>
2025-05-02 19:30:02 +0500

    Modified Facilities , Hero and categories +Added Dropdown

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@ document.querySelectorAll('.cat-item').forEach(el => {
         el.classList.add('selected');
     });
 });
+
+
+
+
+(function () {
+    const avatarBtn = document.getElementById('avatarBtn');
+    const avatarWrapper = document.querySelector('.avatar-dropdown');
+
+    avatarBtn.addEventListener('click', e => {
+        e.stopPropagation();
+        avatarWrapper.classList.toggle('show-menu');
+    });
+
+    document.addEventListener('click', () => {
+        avatarWrapper.classList.remove('show-menu');
+    });
+
+    document.addEventListener('keydown', e => {
+        if (e.key === 'Escape') avatarWrapper.classList.remove('show-menu');
+    });
+})();
